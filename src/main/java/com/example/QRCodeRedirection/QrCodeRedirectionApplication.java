c780db66d3d99d4f8ff7804c21b18a3c845e7ab0
Jeremie Carrel <146066884+Hira-shi@users.noreply.github.com>
2025-02-18 12:02:00 +0100

    Initial Commit

---
+++ src/main/java/com/example/QRCodeRedirection/QrCodeRedirectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.QRCodeRedirection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrCodeRedirectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrCodeRedirectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/QRCodeRedirection/QrCodeRedirectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QRCodeRedirection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCodeRedirectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
